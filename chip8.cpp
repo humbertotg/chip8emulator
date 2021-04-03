d9b9970721d1f8b1282013d337e8b1461de77424
humbertotg <A01196965@itesm.mx>
2021-04-03 17:56:15 -0600

    added missing instructions

--- chip8.cpp
+++ chip8.cpp
@@ -162,14 +162,188 @@ void Chip8::execute()
                     registers[Vx] -= registers[Vy];
                     break;
                 case 0x6:
-                    registers[0xF] = (regiters[Vx] & 0x1);
+                    registers[0xF] = (registers[Vx] & 0x1);
                     registers[Vx] >>= 1;
                     break;
                 case 0x7:
+                    if(registers[Vy] > registers[Vx])
+                    {
+                        registers[0xF] = 1;
+                    }
+                    else 
+                    {
+                        registers[0xF] = 0;
+                    }
                         
+                    registers[Vx] = registers[Vy] - registers[Vx];
+                    break;
+                 case 0xE:
+                    registers[0xF] = (registers[Vx] & 0x80) >> 7;
+                    registers[Vx] <<=1;
+                    break;
+                default:
+                    std::cout<<"error";
+            
             }
             break;
-    }
+        case 0x9:
+            uint8_t Vx = (instruction & 0xF00) >> 8;
+            uint8_t Vy = (instruction & 0xF0) >> 4;
+            if(registers[Vx] != registers[Vy])
+            {
+                pc += 2;
+            }
+            break;
+        case 0xA:
+            uint16_t addr = instruction & 0xFFF;
 
+            index = addr;
+            break;
+        case 0xB:
+            uint16_t addr = instruction & 0xFFF;
+
+            pc = registers[0] + addr;
+            break;
+        //case 0xC: TODO
+        
+        case 0xD:
+            uint8_t Vx = (instruction & 0xF00) >> 8;
+            uint8_t Vy = (instruction & 0xF0) >> 4;
+            uint8_t h = instruction & 0xF;
+
+            uint8_t x = registers[Vx] % 64;
+            uint8_t y = registers[Vy] % 32;
+
+            registers[0xF] = 0;
+            for(unsigned int i = 0;i < h;++i)
+            {
+                uint8_t sByte = memory[index + i];
+
+                for(unsigned int j = 0; j < 8;++j)
+                {
+                    uint8_t sPixel = sByte & (0x80 >> j);
+                    uint32_t* screenPixel = &vid[(y + i) * 64 +(x + j)]; 
+
+                    if(sPixel)
+                    {
+                        if(*screenPixel == 0xFFFFFFFF)
+                        {
+                            registers[0xF] = 1;
+                        }
+
+                        *screenPixel ^= 0xFFFFFFFF;
+                    }
+                }
+            }
+            break;
+        case 0xE:
+            switch(instruction & 0xFF)
+            {
+                case 0x9E:
+                    uint8_t Vx = (instruction & 0xF00) >> 8;
+
+                    uint8_t key = registers[Vx];
+
+                    if(keypad[key])
+                    {
+                        pc += 2;
+                    }
+                    break;
+                case 0xA1:
+                    uint8_t Vx = (instruction & 0xF00) >> 8;
+
+                    uint8_t key = registers[Vx];
+
+                    if(!keypad[key])
+                    {
+                        pc += 2;
+                    }
+                    break;
+                default:
+                    std::cout<<"error";
+            }
+            break;
+        case 0xF:
+            switch(instruction & 0xFF)
+            {
+                case 0x07:
+                    uint8_t Vx = (instruction & 0xF00) >> 8;
+
+                    registers[Vx] = delay_timer;
+                    break;
+                case 0x0A:
+                    uint8_t Vx = (instruction & 0xF00) >> 8;
+                    bool keyPress = false;
+
+                    for(uint8_t i = 0; i < 16; ++i)
+                    {
+                        if(keypad[i])
+                        {
+                            registers[Vx] = i;
+                            keyPress = true;
+                            break;
+                        }
+                    }
+                    if(!keyPress)
+                    {
+                        pc -= 2;
+                    }
+                    break;
+                case 0x15:
+                    uint8_t Vx = (instruction & 0xF00) >> 8;
+
+                    delay_timer = registers[Vx];
+                    break;
+                case 0x18:
+                    uint8_t Vx = (instruction & 0xF00) >> 8;
+
+                    sound_timer = registers[Vx];
+                    break;
+                case 0x1E:
+                    uint8_t Vx = (instruction & 0xF00) >> 8;
+
+                    index += registers[Vx];
+                    break;
+                case 0x29:
+                    uint8_t Vx = (instruction & 0xF00) >> 8;
+
+                    uint8_t digit = registers[Vx];
+                    index = FONT_START_ADDR + (5 * digit);
+                    break;
+                case 0x33:
+                    uint8_t Vx = (instruction & 0xF00) >> 8;
+                    uint8_t num = registers[Vx];
+
+                    memory[index + 2] = num % 10;
+                    num /= 10;
+
+                    memory[index + 1] = num % 10;
+                    num /= 10;
+
+                    memory[index] = value % 10;
+                    break;
+                case 0x55:
+                    uint8_t Vx = (instruction & 0xF00) >> 8;
+
+                    for(uint8_t i = 0; i <= Vx;i++)
+                    {
+                        memory[index + i] = registers[i];
+                    }
+                    break;
+                case 0x65:
+                    uint8_t Vx = (instruction & 0xF00) >> 8;
+
+                    for(uint8_t i = 0; i <= Vx;i++)
+                    {
+                        registers[i] = memory[index + 1];
+                    }
+                    break;
+                    default:
+                    std::cout<<"error";
+            }
+            break;
+        default:
+            std::cout<<"error";
+    }
 }
 
---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "chip8.h"
+
+int main()
+{
+    Chip8 test;
+    
+    while(true)
+    std::cout<<"hello";
+
+}
+
